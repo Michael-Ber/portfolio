75c2a58d43a15e5a6693f9fd97c0e5f2cdf80c63
Michael <example@gmail.com>
2020-08-25 10:55:37 +0800

    fourth screen done

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,32 @@
-const hamburger = document.querySelector('.hamburger'),
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Hamburger on first screen
+    const hamburger = document.querySelector('.hamburger'),
       close = document.querySelector('.menu__close'),
       menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
 
-close.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
+    close.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+
+    // progress bar 4 screen
+
+    const percents = document.querySelectorAll('.skills__progress-percent'),
+          barActive = document.querySelectorAll('.skills__progress-bar-active');
+    
+
+    function stringToNumber (str) {
+        return +str.textContent.slice(0, -1);
+    }
+
+    percents.forEach((percent, index) => {
+        barActive[index].style.width = `${stringToNumber(percent)}%`;
+    });
+})
+
+
+
